77e35d13b64ca951c2453a6980034df86ea15ffd
DreamHost2ws <lightingplays2ws@gmail.com>
2026-05-27 13:42:11 +0530

    Create servers.js

---
+++ launcher/servers.js
@@ -0,0 +1,6 @@
+export const servers = [
+  { name: "BananaSMP", ip: "bananasmp.net" },
+  { name: "DriftSMP", ip: "play.driftsmp.net" },
+  { name: "HappyMC", ip: "play.happymc.fun" },
+  { name: "LightingMC", ip: "play.lightingmc.qzz.io" }
+];
