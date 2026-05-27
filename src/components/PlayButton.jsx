bb4e52e5082ceb08e77787a9a8168793d2d87974
DreamHost2ws <lightingplays2ws@gmail.com>
2026-05-27 13:43:46 +0530

    Update PlayButton.jsx

--- src/components/PlayButton.jsx
+++ src/components/PlayButton.jsx
@@ -1,1 +1,12 @@
+import { launchMinecraft } from "../../launcher/mc-launch";
 
+export default function PlayButton() {
+  return (
+    <button
+      className="bg-gradient-to-r from-yellow-400 to-orange-500 px-12 py-5 rounded-2xl text-black font-bold text-xl hover:scale-105 transition"
+      onClick={() => launchMinecraft("LightingPlayer")}
+    >
+      ▶ PLAY LIGHTING
+    </button>
+  );
+}
