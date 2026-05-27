42f83d6bdc255648e4bd05bb50ce744e09293293
DreamHost2ws <lightingplays2ws@gmail.com>
2026-05-27 13:41:19 +0530

    Update mc-launch.js

--- launcher/mc-launch.js
+++ launcher/mc-launch.js
@@ -1,1 +1,25 @@
+const { Client, Authenticator } = require("minecraft-launcher-core");
 
+function launchMinecraft(username) {
+  const launcher = new Client();
+
+  const opts = {
+    authorization: Authenticator.getAuth(username),
+    root: "./minecraft",
+    version: {
+      number: "1.21.1",
+      type: "release"
+    },
+    memory: {
+      min: "2G",
+      max: "6G"
+    }
+  };
+
+  launcher.launch(opts);
+
+  launcher.on("debug", (e) => console.log("[Lighting] " + e));
+  launcher.on("data", (e) => console.log(e));
+}
+
+module.exports = { launchMinecraft };
