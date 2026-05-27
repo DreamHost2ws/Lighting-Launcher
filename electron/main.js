181a523c759f7e94b2085ab3d869f5f691d3ccd6
DreamHost2ws <lightingplays2ws@gmail.com>
2026-05-27 13:40:36 +0530

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -1,1 +1,19 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 700,
+    backgroundColor: "#0b0f1a",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, "preload.js")
+    }
+  });
+
+  win.loadURL("http://localhost:5173");
+}
+
+app.whenReady().then(createWindow);
